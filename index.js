08da0355281a30666c4566cdbe43e91ad1065dcd
Reem Yacoub <reem.m.yakop@gmail.com>
2024-07-01 10:08:58 +0300

    Fix the six buttons to ensure they perform their respective functions correctly.

---
+++ index.js
@@ -0,0 +1,45 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+// increaseOne()
+// {
+//    homeScore.innerText = homeScore.innerText +1
+//    console.log(homeScore)
+// }
+// document.getElementById("plus-one-point").addEventListener("click", increaseOne);
+
+function increaseOneToHome() {
+    let currentScore = parseInt(homeScore.textContent)
+    let newHomeScore = currentScore +1
+    homeScore.textContent = newHomeScore
+}
+
+function increaseOneToGuest() {
+    let currentScore = parseInt(guestScore.textContent)
+    let newGuestScore = currentScore +1
+    guestScore.textContent = newGuestScore
+}
+
+function increaseTwoToHome() {
+    let currentScore = parseInt(homeScore.textContent)
+    let newHomeScore = currentScore +2
+    homeScore.textContent = newHomeScore
+}
+
+function increaseTwoToGuest() {
+    let currentScore = parseInt(guestScore.textContent)
+    let newGuestScore = currentScore +2
+    guestScore.textContent = newGuestScore
+}
+
+function increaseThreeToHome() {
+    let currentScore = parseInt(homeScore.textContent)
+    let newHomeScore = currentScore +3
+    homeScore.textContent = newHomeScore
+}
+
+function increaseThreeToGuest() {
+    let currentScore = parseInt(guestScore.textContent)
+    let newGuestScore = currentScore +3
+    guestScore.textContent = newGuestScore
+}
